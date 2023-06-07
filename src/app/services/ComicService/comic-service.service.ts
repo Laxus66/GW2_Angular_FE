a71ee629636e6caa5e66aea90fad4cb0a5c95d1a
Laxus66 <Laxus2301@gmail.com>
2023-06-07 23:09:28 +0700

    feat: Services All refs: G2-24

---
+++ src/app/services/ComicService/comic-service.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { IComic } from 'src/app/interfaces/comic';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ComicServiceService {
+
+  constructor(private http: HttpClient) { }
+  getAllComics(): Observable<IComic[]> {
+    return this.http.get<IComic[]>(`http://localhost:8088/comic`)
+  }
+  getOneComic(id: any): Observable<IComic[]> {
+    return this.http.get<IComic[]>(`http://localhost:8088/comic/${id}`)
+  }
+  removeComic(id: any): Observable<IComic[]> {
+    return this.http.delete<IComic[]>(`http://localhost:8088/comic/${id}`)
+  }
+  updateComic(comic: IComic): Observable<IComic[]> {
+    return this.http.put<IComic[]>(`http://localhost:8088/comic/${comic.id}`, comic)
+  }
+  createComic(comic: IComic): Observable<IComic[]> {
+    return this.http.post<IComic[]>(`http://localhost:8088/comic`, comic)
+  }
+}
