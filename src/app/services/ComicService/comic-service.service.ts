82fc7d25e230fbd894a2472ceffafcf82c8aaa25
Laxus66 <Laxus2301@gmail.com>
2023-06-12 00:18:34 +0700

    feat: update route service all refs: G2-24

--- src/app/services/ComicService/comic-service.service.ts
+++ src/app/services/ComicService/comic-service.service.ts
@@ -10,18 +10,18 @@ export class ComicServiceService {
 
   constructor(private http: HttpClient) { }
   getAllComics(): Observable<IComic[]> {
-    return this.http.get<IComic[]>(`http://localhost:8088/comic`)
+    return this.http.get<IComic[]>(`http://localhost:8088/api/comic`)
   }
   getOneComic(id: any): Observable<IComic[]> {
-    return this.http.get<IComic[]>(`http://localhost:8088/comic/${id}`)
+    return this.http.get<IComic[]>(`http://localhost:8088/api/comic/${id}`)
   }
   removeComic(id: any): Observable<IComic[]> {
-    return this.http.delete<IComic[]>(`http://localhost:8088/comic/${id}`)
+    return this.http.delete<IComic[]>(`http://localhost:8088/api/comic/${id}`)
   }
   updateComic(comic: IComic): Observable<IComic[]> {
-    return this.http.put<IComic[]>(`http://localhost:8088/comic/${comic.id}`, comic)
+    return this.http.put<IComic[]>(`http://localhost:8088/api/comic/${comic.id}`, comic)
   }
   createComic(comic: IComic): Observable<IComic[]> {
-    return this.http.post<IComic[]>(`http://localhost:8088/comic`, comic)
+    return this.http.post<IComic[]>(`http://localhost:8088/api/comic`, comic)
   }
 }
