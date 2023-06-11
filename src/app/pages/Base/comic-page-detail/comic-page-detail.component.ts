61cec8d4074d7e069c664940dc6c008f6c95dc91
tiendkph20777 <tiendkph20777@fpt.edu.vn>
2023-06-12 06:40:50 +0700

    update comicDetail

--- src/app/pages/Base/comic-page-detail/comic-page-detail.component.ts
+++ src/app/pages/Base/comic-page-detail/comic-page-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { AuthorServiceService } from 'src/app/services/AuthorService/author-service.service';
+import { ComicServiceService } from 'src/app/services/ComicService/comic-service.service';
 
 @Component({
   selector: 'app-comic-page-detail',
@@ -6,5 +9,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./comic-page-detail.component.scss']
 })
 export class ComicPageDetailComponent {
+  comic: any;
+  author: any;
+  constructor(private comicService: ComicServiceService, private router: ActivatedRoute, private authorService: AuthorServiceService) { }
 
+  ngOnInit() {
+    this.router.params.subscribe((params: any) => {
+      const id = params['id'];
+      this.comicService.getOneComic(id).subscribe((data: any) => {
+        this.comic = data
+        this.authorService.getOneAuthor(data.author_id).subscribe((data: any) => {
+          this.author = data
+          console.log('data :>> ', data);
+        })
+      })
+    })
+  }
 }
